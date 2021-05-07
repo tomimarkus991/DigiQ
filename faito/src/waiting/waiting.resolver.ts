6d2fec9807e41a8a6906edf2f63c9ee8dadda8ee
tomimarkus991 <tomimarkusalber@gmail.com>
2021-05-07 15:31:57 +0300

    add everything for waiting

---
+++ faito/src/waiting/waiting.resolver.ts
@@ -0,0 +1,38 @@
+import { MyContext } from '../types';
+import { Arg, Ctx, Mutation, Resolver } from 'type-graphql';
+import { getConnection } from 'typeorm';
+import { JoinQueueInput } from './dto/join-queue.input';
+
+@Resolver()
+export class WaitingResolver {
+  // Join Queue
+  @Mutation(() => Boolean)
+  // @UseMiddleware(isAuth, isRegularUser)
+  async joinQueue(
+    @Arg('joinQueueInput') joinQueueInput: JoinQueueInput,
+    @Ctx() { req }: MyContext,
+  ): Promise<any> {
+    const { queueId, value } = joinQueueInput;
+    const isWaiting = value !== -1;
+    const realValue = isWaiting ? 1 : -1;
+    let userId = req.session.userId;
+
+    await getConnection().query(
+      `
+      START TRANSACTION;
+
+      insert into userwaiting ("userId", "queueId", value)
+      values(${userId},${queueId},${realValue});
+
+      update queue
+      set waiting = waiting + ${realValue},
+      "shortestWaitingTime" = "estimatedServingtime" * waiting,
+      "longestWaitingTime" = "shortestWaitingTime" + "estimatedServingtime"
+      where id= ${queueId};
+
+      COMMIT;
+      `,
+    );
+    return true;
+  }
+}
