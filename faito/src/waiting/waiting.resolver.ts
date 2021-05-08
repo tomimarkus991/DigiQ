1f742efa7fae79603336543aef90d3413a710e64
tomimarkus991 <tomimarkusalber@gmail.com>
2021-05-08 11:38:35 +0300

    removed unused var

--- faito/src/waiting/waiting.resolver.ts
+++ faito/src/waiting/waiting.resolver.ts
@@ -29,7 +29,7 @@ export class WaitingResolver {
     const realValue = isWaiting ? 1 : -1;
     let userId = req.session.userId;
 
-    const asi = await getConnection().query(
+    await getConnection().query(
       `
       START TRANSACTION;
 
