bc68bf12f70d7aab94eb5505f9183910b907e99f
tomimarkus991 <tomimarkusalber@gmail.com>
2021-05-08 11:34:27 +0300

    added constants for subscription topics

--- faito/src/constants.ts
+++ faito/src/constants.ts
@@ -2,3 +2,7 @@ export const __prod__ = process.env.NODE_ENV === 'production';
 export const __user__ = 'postgres';
 export const __pass__ = 'postgres';
 export const COOKIE_NAME = 'qid';
+
+export const REGISTER_USER = 'REGISTER_USER';
+export const JOIN_QUEUE = 'JOIN_QUEUE';
+export const CREATE_QUEUE = 'CREATE_QUEUE';
