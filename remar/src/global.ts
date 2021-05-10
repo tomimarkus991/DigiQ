131ae9be9e460b192c4c5fc06349dac67cdddb5b
tomimarkus991 <tomimarkusalber@gmail.com>
2021-05-10 16:51:38 +0300

    added global files for fonts and colors, etc

---
+++ remar/src/global.ts
@@ -0,0 +1,16 @@
+export const Colors = {
+  Line: '#b6b6b8',
+  Button_Blue: '#445AE3',
+  Button_Purple: '#882bc2',
+  Text_Placeholder: '#68686b',
+  Text_Header: '#47474a',
+  Text_Input: '#525254',
+  Text_Regular: '#E2E8F0',
+  Text_Gray: '#78787d',
+};
+export const Fonts = {
+  Roboto_300Light: 'Roboto_300Light',
+  Roboto_400Regular: 'Roboto_400Regular',
+  Roboto_500Medium: 'Roboto_500Medium',
+  Roboto_700Bold: 'Roboto_700Bold',
+};
