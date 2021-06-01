699bb28b81314e40691e1dfcb81250cb8eec3028
tomimarkus991 <tomimarkusalber@gmail.com>
2021-06-01 15:54:31 +0300

    add types for best practices

--- faito/src/index.ts
+++ faito/src/index.ts
@@ -51,7 +51,7 @@ const main = async () => {
     session({
       name: COOKIE_NAME,
       store: new RedisStore({
-        client: redis as any,
+        client: redis,
         disableTouch: true,
       }),
       cookie: {
--- faito/src/types.ts
+++ faito/src/types.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { Redis } from 'ioredis';
 
 export type MyContext = {
-  req: Request & { session: any };
+  req: Request & { session: { userId: number } };
   redis: Redis;
   res: Response;
 };
