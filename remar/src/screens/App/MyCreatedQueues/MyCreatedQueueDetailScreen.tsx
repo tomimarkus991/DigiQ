6039753648e21780092f3b692bd124afff33c43f
tomimarkus991 <tomimarkusalber@gmail.com>
2021-06-01 23:23:22 +0300

    redo bad code

--- remar/src/screens/App/MyCreatedQueues/MyCreatedQueueDetailScreen.tsx
+++ remar/src/screens/App/MyCreatedQueues/MyCreatedQueueDetailScreen.tsx
@@ -1,62 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { MyCreatedQueueScreenContent } from '../../../components/myQueueScreen/CreatorRole/MyCreatedQueueScreenContent';
-import { MyQueueScreenContent } from '../../../components/myQueueScreen/UserRole/MyQueueScreenContent';
 import {
   useGetQueueQuery,
   useJoinQueueSubSubscription,
 } from '../../../generated/graphql';
 import { MyColors } from '../../../global';
 import { MyCreatedQueuesNavProps } from '../../../types/MyCreatedQueuesParamList';
-import { MyQueuesNavProps } from '../../../types/MyQueuesParamList';
 
 export const MyCreatedQueueDetailScreen = ({
   route,
   navigation,
 }: MyCreatedQueuesNavProps<'MyCreatedQueue'>) => {
   const id = route.params.id;
-  const hyperData = route.params.newData;
+  const [queueData, setQueueData] = useState<any>();
   const { data } = useGetQueueQuery({
     variables: { id },
+    onCompleted: () => {
+      setQueueData(data?.queue);
+    },
+    fetchPolicy: 'network-only',
   });
-  const { data: newData } = useJoinQueueSubSubscription({
+  const { data: subscriptionData } = useJoinQueueSubSubscription({
     variables: { id },
+    onSubscriptionData: () => {
+      setQueueData(subscriptionData?.joinQueueSub);
+    },
   });
 
   return (
     <View style={styles.main}>
-      {newData?.joinQueueSub ? (
-        <MyCreatedQueueScreenContent
-          data={newData?.joinQueueSub}
-          navigateToQueueScreen={() =>
-            navigation.navigate('PeopleOnTheQueue', { id })
-          }
-          navigateBack={() => navigation.navigate('MyCreatedQueues')}
-          id={id}
-        />
-      ) : (
-        <>
-          {hyperData?.joinQueueSub ? (
-            <MyCreatedQueueScreenContent
-              data={hyperData?.joinQueueSub}
-              navigateToQueueScreen={() =>
-                navigation.navigate('PeopleOnTheQueue', { id })
-              }
-              navigateBack={() => navigation.navigate('MyCreatedQueues')}
-              id={id}
-            />
-          ) : (
-            <MyCreatedQueueScreenContent
-              data={data?.queue}
-              navigateToQueueScreen={() =>
-                navigation.navigate('PeopleOnTheQueue', { id })
-              }
-              navigateBack={() => navigation.navigate('MyCreatedQueues')}
-              id={id}
-            />
-          )}
-        </>
-      )}
+      <MyCreatedQueueScreenContent
+        data={queueData}
+        navigateToQueueScreen={() =>
+          navigation.navigate('PeopleOnTheQueue', { id })
+        }
+        navigateBack={() => navigation.navigate('MyCreatedQueues')}
+        id={id}
+      />
     </View>
   );
 };
