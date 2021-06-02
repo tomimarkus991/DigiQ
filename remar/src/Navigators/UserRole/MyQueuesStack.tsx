029db030e6234c547d6fe19bcac8ac738a1c15da
tomimarkus991 <tomimarkusalber@gmail.com>
2021-06-02 16:01:37 +0300

    added custom header

--- remar/src/Navigators/UserRole/MyQueuesStack.tsx
+++ remar/src/Navigators/UserRole/MyQueuesStack.tsx
@@ -1,9 +1,10 @@
+import { AntDesign } from '@expo/vector-icons';
 import {
   createStackNavigator,
   TransitionSpecs,
 } from '@react-navigation/stack';
 import React from 'react';
-import { Easing } from 'react-native';
+import { Easing, TouchableOpacity } from 'react-native';
 import { MyQueueDetailScreen } from '../../screens/App/MyQueues/MyQueueDetailScreen';
 import { MyQueuesScreen } from '../../screens/App/MyQueues/MyQueuesScreen';
 import {
@@ -13,7 +14,9 @@ import {
 
 const Stack = createStackNavigator<MyQueuesParamList>();
 
-export const MyQueuesStack = ({}: MyQueuesNavProps<'MyQueues'>) => {
+export const MyQueuesStack = ({
+  navigation,
+}: MyQueuesNavProps<'MyQueues'>) => {
   return (
     <Stack.Navigator initialRouteName="MyQueues">
       <Stack.Screen
@@ -27,6 +30,27 @@ export const MyQueuesStack = ({}: MyQueuesNavProps<'MyQueues'>) => {
         options={{
           headerTitle: '',
           headerTransparent: true,
+          headerLeft: () => (
+            <TouchableOpacity
+              style={{
+                marginLeft: 10,
+                marginTop: 10,
+                backgroundColor: '#fff',
+                borderRadius: 30,
+                padding: 5,
+              }}
+              onPress={() => {
+                navigation.navigate('MyQueues');
+              }}
+            >
+              <AntDesign
+                name="leftcircle"
+                size={42}
+                color="black"
+                style={{ backgroundColor: '#fff', borderRadius: 100 }}
+              />
+            </TouchableOpacity>
+          ),
           gestureEnabled: true,
           gestureDirection: 'horizontal',
           transitionSpec: {
